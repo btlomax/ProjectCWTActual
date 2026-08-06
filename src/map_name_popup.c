a2ad6b4fe255f42465b7b74f3d4a67dd9f085fa1
Ben Lomax <lomaxb@hotmail.co.uk>
2026-08-06 20:05:02 +0100

    Deleted FRLG maps

--- src/map_name_popup.c
+++ src/map_name_popup.c
@@ -545,19 +545,7 @@ static void MapNamePopupAppendFloorNum(u8 *map_name, s8 floorNum)
 
 static bool32 IsCeladonDeptStore(const struct MapHeader *mapHeader)
 {
-    if (mapHeader->regionMapSectionId != MAPSEC_CELADON_CITY)
-        return FALSE;
-    if (mapHeader->mapLayoutId != LAYOUT_CELADON_CITY_DEPARTMENT_STORE_1F
-     && mapHeader->mapLayoutId != LAYOUT_CELADON_CITY_DEPARTMENT_STORE_2F
-     && mapHeader->mapLayoutId != LAYOUT_CELADON_CITY_DEPARTMENT_STORE_3F
-     && mapHeader->mapLayoutId != LAYOUT_CELADON_CITY_DEPARTMENT_STORE_4F
-     && mapHeader->mapLayoutId != LAYOUT_CELADON_CITY_DEPARTMENT_STORE_5F
-     && mapHeader->mapLayoutId != LAYOUT_CELADON_CITY_DEPARTMENT_STORE_ROOF
-     && mapHeader->mapLayoutId != LAYOUT_CELADON_CITY_DEPARTMENT_STORE_ELEVATOR)
-    {
-        return FALSE;
-    }
-    return TRUE;
+   return FALSE; // Disabled for now, as the map name is too long to fit in the popup window.
 }
 
 u8 *GetPopUpMapName(u8 *dest, const struct MapHeader *mapHeader)
