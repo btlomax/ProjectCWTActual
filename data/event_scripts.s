3c0e3943c4ccb06ec6df7246bc0100ecdaef5cbb
Ben Lomax <lomaxb@hotmail.co.uk>
2026-08-14 20:51:30 +0100

    Got connection between Route 1 and Alderbrook Town

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1329,3 +1329,5 @@ EventScript_PalletTown_PlayersHouse_2F_TurnOnPC::
 	.include "data/maps/NPC_House_Inside/scripts.inc"
 
 	.include "data/maps/PlayerHouse_Inside_Upstairs/scripts.inc"
+
+	.include "data/maps/Route1_WillbendCanal/scripts.inc"
