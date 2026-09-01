376623aac8b7ff42d0c1585c0a80960bf9db379f
Ben Lomax <lomaxb@hotmail.co.uk>
2026-09-01 22:19:15 +0100

    First trainer done, calling it a night

--- include/constants/opponents.h
+++ include/constants/opponents.h
@@ -864,12 +864,13 @@
 #define TRAINER_ROUTE2_MEADOWVALE_JENNY     857
 #define TRAINER_ROUTE2_MEADOWVALE_LUCY      858
 #define TRAINER_ROUTE2_MEADOWVALEFARM_INSIDE_MAX 859
+#define TRAINER_WEIRFORDFOREST_BUGCATCHER_SAM 860
 
 // NOTE: Because each Trainer uses a flag to determine when they are defeated, there is only space for 9 additional trainers before trainer flag space overflows
 //       More space can be made by shifting flags around in constants/flags.h or changing how trainer flags are handled
 //       MAX_TRAINERS_COUNT can be increased but will take up additional saveblock space
 
-#define TRAINERS_COUNT_EMERALD     860
+#define TRAINERS_COUNT_EMERALD     861
 #define MAX_TRAINERS_COUNT_EMERALD 864
 
 #if IS_FRLG
