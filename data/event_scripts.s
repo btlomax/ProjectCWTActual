6eb09a76761010da071cf70a4e090121fe84b647
Ben Lomax <lomaxb@hotmail.co.uk>
2026-09-13 12:10:31 +0100

    Route3 layout mostly done

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1339,3 +1339,5 @@ EventScript_PalletTown_PlayersHouse_2F_TurnOnPC::
 	.include "data/maps/Meadowvale_Farm_Inside/scripts.inc"
 
 	.include "data/maps/Pokemon_Tutor_Inside/scripts.inc"
+
+	.include "data/maps/Route3_Actual/scripts.inc"
